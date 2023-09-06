f122893b6e1243d83d7eeff7d0ea93e3b3dda717
Ishara Madusanka <isharamadusanka410@gmail.com>
2023-09-07 02:37:19 +0530

    main quiz home done

--- src/router/Router.jsx
+++ src/router/Router.jsx
@@ -3,13 +3,14 @@ import { Routes, Route } from "react-router-dom";
 
 // Import React components
 import NotFound from "../pages/NotFound";
-import Home from "../pages/Home";
+// import Home from "../pages/Home";
+import MainQuizHome from "../pages/mainQuiz/MainQuizHome";
 
 const Router = () => {
   return (
     <Routes>
       {/* App routes*/}
-      <Route path="/" element={<Home />} />
+      <Route path="/" element={<MainQuizHome />} />
 
       {/* Handle a 404 Not Found route */}
       <Route path="*" element={<NotFound />} />
