5f7b5b587fe35f9e099557eff2ecfbdf5e6d8e1d
Yasiru Deshan <rpyasiru@gmail.com>
2023-09-06 19:16:13 +0530

    nearsighted home and test button link added

---
+++ src/pages/yasiru/NearSightedTestView.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const NearSightedTestView = () => {
+  return (
+    <div>test view</div>
+  )
+}
+
+export default NearSightedTestView
--- src/router/Router.jsx
+++ src/router/Router.jsx
@@ -4,6 +4,8 @@ import { Routes, Route } from "react-router-dom";
 // Import React components
 import NotFound from "../pages/NotFound";
 import Home from "../pages/Home";
+import NearSighted from "../pages/yasiru/NearSighted";
+import NearSightedTestView from "../pages/yasiru/NearSightedTestView";
 
 const Router = () => {
   return (
@@ -13,6 +15,15 @@ const Router = () => {
 
       {/* Handle a 404 Not Found route */}
       <Route path="*" element={<NotFound />} />
+
+
+      {/* -----------Yasiru Routes--------------------- */}
+      <Route path="/near-sighted" element={<NearSighted />} />
+      <Route path="/test-view" element={<NearSightedTestView />} />
+
+
+
+
     </Routes>
   );
 };
