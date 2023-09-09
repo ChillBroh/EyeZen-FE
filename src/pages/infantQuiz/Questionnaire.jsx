cd2b5648a21ca1dd755d076315de6711310c5fad
umeshadilshani <umeshadilshani723@gmail.com>
2023-09-09 15:24:32 +0530

    Added pdf generation

--- src/pages/infantQuiz/Questionnaire.jsx
+++ src/pages/infantQuiz/Questionnaire.jsx
@@ -1,12 +1,14 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
-import result from "../../assets/infantVisionImg/result.jpg"
+import result from "../../assets/infantVisionImg/result.jpg";
+import jsPDF from "jspdf";
 
 const Questionnaire = () => {
   const [questions, setQuestions] = useState([]);
   const [currentQuestionIndex, setCurrentQuestionIndex] = useState(0);
   const [userAnswers, setUserAnswers] = useState({});
   const [score, setScore] = useState(null);
+  const [percentage, setPercentage] = useState("");
 
   useEffect(() => {
     // Fetch quiz questions from the backend when the component mounts
@@ -55,11 +57,66 @@ const Questionnaire = () => {
       .post("http://localhost:5000/api/infantQuiz/check", { userAnswers })
       .then((response) => {
         setScore(response.data.score);
+        // Calculate the percentage here
+        const calculatedPercentage = (response.data.score / questions.length) * 100;
+        setPercentage(calculatedPercentage);
       });
   };
 
   const handleButtonClick = () => {
-    window.location.href = '/infant_quiz'; // Change the URL to navigate to the '/quiz' route
+    window.location.href = "/infant_quiz"; // Change the URL to navigate to the '/quiz' route
+  };
+
+  const generatePDF = () => {
+    const doc = new jsPDF();
+  
+    // Define styles
+    const titleStyle = { fontSize: 24, textColor: [0, 0, 255] }; // Blue color
+    const subtitleStyle = { fontSize: 18 };
+    const scoreStyle = { fontSize: 18, textColor: [255, 0, 0] }; // Red color
+    const suggestionStyle = { fontSize: 12 };
+    const footerStyle = { fontSize: 10, textColor: [128, 128, 128] }; // Gray color
+  
+    // Measure the width of the subtitle text
+    const subtitleText = "Infant Vision Test Results";
+    const subtitleWidth = doc.getStringUnitWidth(subtitleText) * subtitleStyle.fontSize / doc.internal.scaleFactor;
+  
+    // Calculate the X position to center the subtitle
+    const pageWidth = doc.internal.pageSize.width;
+    const subtitleX = (pageWidth - subtitleWidth) / 2;
+  
+    // Add title
+    doc.setTextColor.apply(doc, titleStyle.textColor);
+    doc.setFontSize(titleStyle.fontSize);
+    doc.text("EyeZen", subtitleX, 10);
+  
+    // Add subtitle centered horizontally
+    doc.setTextColor(0); // Reset text color to black
+    doc.setFontSize(subtitleStyle.fontSize);
+    doc.text(subtitleText, subtitleX, 20);
+  
+    // Add score
+    doc.setTextColor.apply(doc, scoreStyle.textColor);
+    doc.setFontSize(scoreStyle.fontSize);
+    doc.text(`Score Obtained: ${percentage.toFixed(2)}%`, 10, 30);
+  
+    // Add suggestions
+    doc.setFontSize(suggestionStyle.fontSize);
+    doc.text("Vision Condition:", 10, 40);
+    if (percentage <= 50) {
+      doc.text("Vision seems to be a bit weak", 20, 50);
+      doc.text("Recommendation: Meet an ophthalmologist", 20, 60);
+    } else {
+      // Add other conditions and recommendations as needed
+    }
+  
+    // Add footer
+    doc.setTextColor.apply(doc, footerStyle.textColor);
+    doc.setFontSize(footerStyle.fontSize);
+    doc.text("EyeZen Infant Vision Test", 10, 280);
+  
+    // Save the PDF or open it in a new tab
+    doc.save("infant_vision_test_results.pdf");
   };
 
   const renderQuestion = () => {
@@ -102,34 +159,40 @@ const Questionnaire = () => {
   };
 
   const displayScore = () => {
-    const percentage = (score / questions.length) * 100;
-
-    return(
+    return (
       <div>
         {score !== null && (
-        <div className="w-3/4 mx-auto mt-20 flex gap-8 mb-20">
-          <div className="w-full">
-            <h1 className="text-bold text-4xl mb-5">Test Results</h1>
-            <h2 className="text-2xl mt-4 text-blue-600 text-bold mb-10">Score Obtained: {percentage}<nbsp/>%</h2>
-            <h3 className="text-xl text-bold mb-3">Vision Condition</h3>
-            {percentage <= 50 && (
-              <ul className="list-disc">
-                <li>Vision seems to be bit week</li>
-                <li>Recommend to meet an ophthalmologist</li>
-              </ul>
-            )}
+          <div className="w-3/4 mx-auto mt-20 flex gap-8 mb-20">
+            <div className="w-full">
+              <h1 className="text-bold text-4xl mb-5">Test Results</h1>
+              <h2 className="text-2xl mt-4 text-blue-600 text-bold mb-10">
+                Score Obtained: {percentage.toFixed(2)}%
+              </h2>
+              <h3 className="text-xl text-bold mb-3">Vision Condition</h3>
+              {percentage <= 50 && (
+                <ul className="list-disc">
+                  <li>Vision seems to be a bit weak</li>
+                  <li>Recommendation: Meet an ophthalmologist</li>
+                </ul>
+              )}
+            </div>
+            <div>
+              <img src={result} alt="Result" className="w-full rounded-3xl" />
+            </div>
+            {/* Additional feedback or results can be displayed here */}
           </div>
-          <div>
-            <img src={result} alt="Result" className="w-full rounded-3xl"/>
-          </div>
-          {/* Additional feedback or results can be displayed here */}
-        </div>
-      )}
+        )}
         <button
           onClick={handleButtonClick}
-          className="px-10 ml-40 py-3 bg-blue-600 text-white rounded-3xl hover:bg-slate-800 mb-20"
+          className="px-10 ml-40 py-3 bg-blue-600 text-white rounded-3xl hover:bg-slate-800 mb-20 mr-10"
         >
           Back To Home Page
+        </button>
+        <button
+          onClick={generatePDF}
+          className="px-10 py-3 bg-blue-600 text-white rounded-3xl hover-bg-slate-800 mb-20"
+        >
+          Generate PDF
         </button>
       </div>
     );
@@ -139,39 +202,35 @@ const Questionnaire = () => {
     <div>
       {score === null && (
         <div className="text-center mt-20">
-        {renderQuestion()}
-        <div className="mt-10 mb-20">
-          {currentQuestionIndex > 0 && (
-            <button
-              onClick={handlePreviousQuestion}
-              className="px-20 py-3 bg-gray-500 text-white rounded-3xl hover:bg-gray-600 mr-2"
-            >
-              Previous
-            </button>
-          )}
-          {currentQuestionIndex < questions.length - 1 ? (
-            <button
-              onClick={handleNextQuestion}
-              className="px-20 py-3 bg-black text-white rounded-3xl hover:bg-slate-800"
-            >
-              Skip
-            </button>
-          ) : (
-            <button
-              onClick={handleSubmitAnswers}
-              className="px-20 py-3 bg-green-500 text-white rounded-3xl hover:bg-green-600"
-            >
-              Submit
-            </button>
-          )}
-        </div>
-      </div>
-      )}
-      {score !== null &&(
-        <div>
-          {displayScore()}
+          {renderQuestion()}
+          <div className="mt-10 mb-20">
+            {currentQuestionIndex > 0 && (
+              <button
+                onClick={handlePreviousQuestion}
+                className="px-20 py-3 bg-gray-500 text-white rounded-3xl hover:bg-gray-600 mr-2"
+              >
+                Previous
+              </button>
+            )}
+            {currentQuestionIndex < questions.length - 1 ? (
+              <button
+                onClick={handleNextQuestion}
+                className="px-20 py-3 bg-black text-white rounded-3xl hover:bg-slate-800"
+              >
+                Skip
+              </button>
+            ) : (
+              <button
+                onClick={handleSubmitAnswers}
+                className="px-20 py-3 bg-green-500 text-white rounded-3xl hover:bg-green-600"
+              >
+                Submit
+              </button>
+            )}
+          </div>
         </div>
       )}
+      {score !== null && <div>{displayScore()}</div>}
     </div>
   );
 };
