cd35182259689d92688112068fb97482dd5757ce
Ishara Madusanka <isharamadusanka410@gmail.com>
2023-09-12 14:43:05 +0530

    mainquiz home done

--- src/pages/mainQuiz/MainQuizHome.jsx
+++ src/pages/mainQuiz/MainQuizHome.jsx
@@ -1,47 +1,10 @@
 import StepperComponent from "../../components/StepperComponent.jsx";
 import Button from "../../components/Button";
 import { Link } from "react-router-dom";
+import Data from "../../assets/data.js";
 
 const MainQuizHome = () => {
-  const quizDeatils = [
-    {
-      id: "questionOne",
-      question: "What is Ayurvedic eye care?",
-      answer:
-        "Ayurvedic eye care is a holistic approach to maintaining and improving eye health using principles from Ayurveda, an ancient Indian system of medicine. It involves natural remedies, dietary recommendations, eye exercises, and lifestyle practices to promote healthy vision.",
-    },
-    {
-      id: "questionTwo",
-      question: "Can Ayurveda help with common eye issues?",
-      answer:
-        "Yes, Ayurveda offers solutions for common eye issues such as dry eyes, eye strain, and redness. Ayurvedic treatments may include herbal eye drops, eye exercises, and dietary changes to alleviate these problems.",
-    },
-    {
-      id: "questionThree",
-      question: "Are there Ayurvedic remedies for improving eyesight?",
-      answer:
-        "Ayurveda provides techniques to improve eyesight naturally. These may include the use of herbal formulations, regular eye exercises, and maintaining a balanced diet rich in eye-friendly nutrients.",
-    },
-    {
-      id: "questionFour",
-      question: "How can I reduce eye strain with Ayurvedic practices?",
-      answer:
-        "Ayurveda recommends practices like Palming exercises, Trataka meditation, and using herbal eye drops to reduce eye strain caused by prolonged screen time or reading. These techniques help relax the eye muscles and relieve strain.",
-    },
-    {
-      id: "questionFive",
-      question:
-        "What dietary recommendations does Ayurveda suggest for eye health?",
-      answer:
-        "Ayurveda emphasizes a diet rich in vitamin A, vitamin C, and antioxidants to support eye health. Foods like carrots, Indian gooseberries (Amla), spinach, and almonds are considered beneficial for the eyes.",
-    },
-    {
-      id: "questionSix",
-      question: "Is it necessary to consult an Ayurvedic eye doctor?",
-      answer:
-        "While Ayurvedic remedies can be helpful, it's essential to consult with a qualified Ayurvedic eye doctor or practitioner before starting any treatment, especially for serious eye conditions. They can provide personalized guidance based on your specific needs.",
-    },
-  ];
+  const quizDeatils = Data;
   return (
     <div className="mx-auto max-w-2xl mt-24 px-4  sm:px-6  lg:max-w-7xl lg:px-8">
       <div>
@@ -51,19 +14,22 @@ const MainQuizHome = () => {
         </div>
       </div>
       <div className="mt-16">
-        Lorem Ipsum is simply dummy text of the printing and typesetting
-        industry. Lorem Ipsum has been the industry's standard dummy text ever..
-        Lorem Ipsum is simply dummy text of the printing and typesetting
-        industry. Lorem Ipsum has been the industry's standard dummy text
-        ever.Lorem Ipsum is simply dummy text of the printing and typesetting
-        industry. Lorem Ipsum has been the industry's standard dummy text
-        ever.....
+        Welcome to the Vision Test, a comprehensive assessment designed to
+        evaluate your visual acuity and detect common vision problems. This test
+        aims to provide you with valuable insights into your eye health. It's a
+        simple yet effective way to gauge your vision and identify potential
+        issues that may require further attention.....
       </div>
       <div>
-        <div className="font-semibold text-2xl mt-16">About the Test</div>
         <div className="mx-auto max-w-2xl px-4  sm:px-4 sm:py-15 lg:max-w-7xl lg:px-8">
           <StepperComponent data={quizDeatils} />
         </div>
+      </div>
+      <div className="mb-5 text-center font-bold">
+        <h1>
+          Thank you for choosing our Vision Test. Let's get started on the
+          journey to better eye health!
+        </h1>
       </div>
       <div className="flex justify-center mb-10">
         <Link to="/main-questions">
